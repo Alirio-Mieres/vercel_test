a0b150f2f616e62b277a80315293e57d76dd2be9
Alirio Mieres <mie21001@byui.edu>
2023-07-21 14:08:50 -0400

    fet (Home) search input

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,64 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  // const cursos = ["Programacion", "Inglés", "Aire Acondicionado", "Java"];
+  const cursos = [
+    "Programación Full Stack",
+    "Inglés para Call Center",
+    "Aire Acondicionado",
+    "Java",
+    "Mecánica de motos",
+    "Asesor Comercial",
+    "Conectividad y Redes",
+    "Asesor Financiero",
+    "Auxiliar de Almacén y Logística",
+    "Mecánica de Motos",
+    "Marketing Digital",
+    "Visitador Médico",
+    "Farmacología",
+    "Asistente Administrativo",
+    "Mantenimiento y reparación de celulares, tablets y computadoras",
+    "Mantenimiento y reparación de aires acondicionados y electricidad básica",
+  ];
+  //Estado que me ayudara a mostrar los cursos filtrados
+  const [search, setSearch] = useState(cursos);
+
+  // Funcion que maneja el cambio en el input
+  const handleSearch = (event) => {
+    //Guardo el texto del input
+    let text = event.target.value;
+    const filtered = [];
+
+    //Itero el arreglo con todos los cursos y los filtros
+    cursos.forEach((element) => {
+      //Convierto a minusculas los strings de los cursos y corroboro si
+      //incluye el texto del input
+      if (element.toLowerCase().includes(text)) {
+        //Guardo el elemento que coincida con la búsqueda
+        filtered.push(element);
+      }
+    });
+
+    //Cambio el estado para mostrar los cusos filtrados
+    if (text === "") {
+      setSearch([]);
+      return;
+    }
+
+    // cambio el estado para mostrar los cursos filtrados
+    setSearch(filtered);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <input type="text" onKeyUp={handleSearch} />
+      <ul>
+        {search.map((element, i) => (
+          <li key={i}>{element}</li>
+        ))}
+      </ul>
     </main>
-  )
+  );
 }
